268797c1b88c3d4c0a885ff442ffbe2c82a11fe7
Ralf Schmitt <ralf@systemexit.de>
2010-09-01 15:37:51 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='qserve',
+      author="Ralf Schmitt",
+      author_email="schmir@gmail.com",
+      version="0.0.1",
+      url="http://github.com/schmir/qserve",
+      description="job queue server",
+      license="BSD License",
+      packages=["qs"])
