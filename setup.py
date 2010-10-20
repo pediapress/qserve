3081182cc3c4c782be03f8d8c419dff972731dde
Ralf Schmitt <ralf@systemexit.de>
2010-10-20 12:13:29 +0200

    setup.py: set maintainer.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 
 from distutils.core import setup
 setup(name='qserve',
-      author="Ralf Schmitt",
-      author_email="schmir@gmail.com",
       version="0.0.1",
       url="http://github.com/schmir/qserve",
       description="job queue server",
       license="BSD License",
+      maintainer="pediapress.com",
+      maintainer_email="info@pediapress.com",
       packages=["qs"])
