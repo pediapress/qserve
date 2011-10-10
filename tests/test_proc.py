9393a16d7220a582d19927150725c53789c834e4
Ralf Schmitt <ralf@systemexit.de>
2011-10-10 16:07:45 +0200

    add some basic tests for qs.proc

---
+++ tests/test_proc.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env py.test
+
+import sys, time
+from qs import proc
+
+
+def test_run_cmd_with_this():
+    st, out = proc.run_cmd([sys.executable, "-c" "import this"])
+    assert "Namespaces are one honking great idea -- let's do more of those!" in out
+
+
+def test_run_cmd_timeout():
+    stime = time.time()
+    st, out = proc.run_cmd([sys.executable, "-c" "import time; time.sleep(10)"], 0.5)
+    needed = time.time() - stime
+    assert (st, out) == (9, "")
+    assert needed >= 0.5
+    assert needed < 1.0
