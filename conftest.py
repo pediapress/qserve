7b1a7c52d3c31366abf36bba7c413eed28cb45b0
Ralf Schmitt <ralf@systemexit.de>
2011-10-11 12:27:49 +0200

    report gevent and greenlet version when running py.test

---
+++ conftest.py
@@ -0,0 +1,8 @@
+
+import gevent
+import greenlet
+
+def pytest_report_header(config):
+    return "gevent %s  --  greenlet %s" % (
+        ".".join([str(x) for x in gevent.version_info]),
+        greenlet.__version__)
