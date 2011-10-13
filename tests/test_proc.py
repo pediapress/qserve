8dd3287342e8fd1b79ad958ec34e4ffba2240f85
Ralf Schmitt <ralf@systemexit.de>
2011-10-13 14:41:54 +0200

    make test_proc execute a bit faster
    we just sleep a bit less and lower the timeouts.

--- tests/test_proc.py
+++ tests/test_proc.py
@@ -11,12 +11,12 @@ def test_run_cmd_with_this():
 
 def test_run_cmd_timeout():
     stime = time.time()
-    st, out = proc.run_cmd([sys.executable, "-c" "import time; time.sleep(10)"], 0.5)
+    st, out = proc.run_cmd([sys.executable, "-c" "import time; time.sleep(10)"], 0.2)
     needed = time.time() - stime
     assert (st, out) == (9, "")
-    assert needed >= 0.49
-    assert needed < 1.0
+    assert needed >= 0.18
+    assert needed < 0.4
 
 
 def test_run_cmd_trigger_loopexit():
-    proc.run_cmd([sys.executable, "-uc", "import time, os, this; os.close(1); os.close(2); time.sleep(1)"])
+    proc.run_cmd([sys.executable, "-uc", "import time, os, this; os.close(1); os.close(2); time.sleep(0.2)"])
