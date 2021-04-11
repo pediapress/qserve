dde7024778173280d23f4030d4e7d77d8d9e49e9
Christoph Kepper <christoph.kepper@pediapress.com>
2021-04-11 13:12:38 +0200

    modernize setup.py and simplify __init__.py
    * move version to setup.py instead of using exec to load it

--- setup.py
+++ setup.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python
+from setuptools import setup, find_packages
 
 
-def get_version():
-    d = {}
-    filename = "qs/__init__.py"
-    exec(compile(open(filename, "rb").read(), filename, "exec"), d, d)
-    return d["__version__"]
-
-
-if __name__ == "__main__":
-    try:
-        from setuptools import setup
-    except ImportError:
-        from distutils.core import setup
-    setup(
-        name="qserve",
-        version=get_version(),
-        url="https://github.com/pediapress/qserve",
-        description="job queue server",
-        license="BSD License",
-        install_requires=["future"],
-        maintainer="pediapress.com",
-        maintainer_email="info@pediapress.com",
-        packages=["qs"],
-    )
+setup(
+    description="job queue server",
+    install_requires=["future"],
+    license="BSD License",
+    maintainer="pediapress.com",
+    maintainer_email="info@pediapress.com",
+    name="qserve",
+    packages=find_packages(),
+    url="https://github.com/pediapress/qserve",
+    version="0.3.0",
+)
